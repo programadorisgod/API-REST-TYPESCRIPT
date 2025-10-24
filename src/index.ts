633e7b1036c4faa789d239c9563d6a520d26ba17
programadorisgod <catapiascastro@gmail.com>
2025-10-24 17:03:24 -0500

    update: add morgan middleware to logs request

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,32 @@
-import express, { json, urlencoded } from 'express'
-import cors from 'cors'
+import express, { json, urlencoded } from "express";
+import cors from "cors";
 
+import { findPort } from "./config/port";
+import { connectionDB } from "./config/connection";
+import routerUser from "./routes/user";
+import morgan from "morgan";
 
-import { findPort } from './config/port'
-import { connectionDB } from './config/connection'
-import routerUser from './routes/user'
+const app = express();
+const PORT: string | number = process.env.PORT ?? 4000;
 
+app.disable("x-powered-by");
+app.use(cors({ origin: "*" }));
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(morgan("dev"));
 
-const app = express()
-const PORT:string | number = process.env.PORT ?? 4000
+app.get("/", (req, res) => {
+  const data = { metodo: req.method, url: req.url };
 
-app.disable('x-powered-by')
-app.use(cors({origin: '*'}))
-app.use(urlencoded({ extended: true }))
-app.use(json())
+  res.status(200).json({ msg: "Welcome to my api TS", data });
+});
 
+app.use(routerUser);
 
+connectionDB();
 
-app.get('/', (req, res) => {
-
-  const data = {metodo: req.method, url :req.url }
-
-  res.status(200).json({msg: 'Welcome to my api TS', data })
-
-})
-
-app.use(routerUser)
-
-
-connectionDB()
-
-findPort(PORT).then(port => {
-  app.listen(port, () => console.log(`App running in the port: ${port}`))
-})
-.catch(err => console.log('Erorr:', err))
- 
+findPort(PORT)
+  .then((port) => {
+    app.listen(port, () => console.log(`App running in the port: ${port}`));
+  })
+  .catch((err) => console.log("Erorr:", err));
