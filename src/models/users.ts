04cbb5a1779d995f8ed2ffba425b02bca71f73e5
programadorisgod <catapiascastro@gmail.com>
2025-10-24 17:02:48 -0500

    fix: change structure model user

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,65 +1,77 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  cliente : {
-    type: String,
-    unique: true,
-    required: true
+const userSchema = new Schema(
+  {
+    id: {
+      type: Schema.Types.UUID,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    measueres: {
+      AE: {
+        type: Number,
+        required: true,
+      },
+      TD: {
+        type: Number,
+        required: true,
+      },
+      TE: {
+        type: Number,
+        required: true,
+      },
+      CP: {
+        type: Number,
+        required: true,
+      },
+      ALB: {
+        type: Number,
+        required: true,
+      },
+      SB: {
+        type: Number,
+        required: true,
+      },
+      CC: {
+        type: Number,
+        required: true,
+      },
+      CK: {
+        type: Number,
+        required: true,
+      },
+      ALK: {
+        type: Number,
+        required: true,
+      },
+      LT: {
+        type: Number,
+        required: true,
+      },
+      LM: {
+        type: Number,
+        required: true,
+      },
+      LSH: {
+        type: Number,
+        required: true,
+      },
+    },
   },
-  AE: {
-    type: Number,
-    required: true
-  },
-  TD: {
-    type: Number,
-    required: true
-  },
-  TE: {
-    type: Number,
-    required: true
-  },
-  CP: {
-    type: Number,
-    required: true
-  },
-  ALB: {
-    type: Number,
-    required: true
-  },
-  SB: {
-    type: Number,
-    required: true
-  },
-  CC: {
-    type: Number,
-    required: true
-  },
-  CK: {
-    type: Number,
-    required: true
-  },
-  ALK: {
-    type: Number,
-    required: true
-  },
-  LT: {
-    type: Number,
-    required: true
-  },
-  LM: {
-    type: Number,
-    required: true
-  },
-  LSH: {
-    type: Number,
-    required: true
-  },
-}, 
-{ timestamps: true, 
-  versionKey: false 
-}
-)   
+  { timestamps: true, versionKey: false },
+);
 
-const User = model('User', userSchema)
+const User = model("User", userSchema);
 
-export default User
+export default User;
